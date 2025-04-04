3bcaf009c26f3f0f871e1067300f913821885135
san-bin-hoque <techakjak@gmail.com>
2025-04-04 15:48:50 +0600

    log remove

--- src/action/loginCasino.ts
+++ src/action/loginCasino.ts
@@ -54,7 +54,7 @@ export const logInCasino = async (data: {
       });
     }
 
-    console.log({ response });
+   
     if (response?.error === "0") {
       return { error: INTERNAL_SERVER_ERROR };
     }

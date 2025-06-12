34a84e298079dfa4f28115f5ca0a4042dc0571cc
Epti <techakjak@gmail.com>
2025-06-11 19:11:19 -0700

    Removed Invalid Route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import { redirect } from "next/navigation";
+"use client";
+import Footer from "@/components/landing/footer/Footer";
+import Header from "@/components/landing/headers/Header";
+import Hero from "@/components/landing/hero/Hero";
+import Live from "@/components/casino/live";
+import Slots from "@/components/casino/Slots";
+import TabBar from "@/components/landing/TabBar";
 
-const App = () => {
-  return redirect("/en");
-};
-
-export default App;
+export default function Home() {
+  return (
+    <div className=" min-h-screen">
+      <Header />
+      <main className="container">
+        <Hero />
+        <Slots />
+        <Live />
+        <Footer />
+      </main>
+      <TabBar />
+    </div>
+  );
+}
